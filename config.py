107b0fabd8f0b7226040b4b293e6e01d3b9dbbb3
dvtarkov <daniil.tarkov@gmail.com>
2024-07-05 04:06:01 +0500

    dotenv deleted

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
 import json
 
-from dotenv import load_dotenv
-
-# Загружаем переменные окружения из .env файла
-load_dotenv()
-
 with open("conf.json", 'r') as config:
     REPOS = dict(json.load(config))
